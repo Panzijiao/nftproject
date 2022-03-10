fd9418863062e01d6c501085bf893b4f353a1256
Panzijiao <59632892+Panzijiao@users.noreply.github.com>
2022-03-10 19:40:43 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const CONFIG = {
     "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "Axie Infinity",
+    "title": "AxieInfinity",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
